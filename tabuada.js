37ce3d964ae1e9a641fd70c066ecb76ed81cf4e9
whoispond <anak.desouzalopes17@gmail.com>
2023-03-15 02:22:57 -0300

    tentei colocar função, falhei

---
+++ tabuada.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var num = parseInt(document.getElementById("num").value);
+    var resposta = document.getElementsById("resposta");
+    var calcular = '';
+
+    for (var count = 1; count <= 10; count++);
+    calcular += num + "x" + count + "=" + num * count + "<br>";
+    resposta.innerHTML = calcular;
+
+}
